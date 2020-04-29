53f597fb275d0d8afde2fcd5055fe7de49d81d3c
flaviodiminuto <flavio.diminuto@outlook.com>
2020-04-29 01:22:12 -0300

    [update] adicionando Swagger

---
+++ src/main/java/com/flaviodiminuto/corre/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.flaviodiminuto.corre.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.annotations.Info;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket api() {
+		ApiInfo info = new ApiInfoBuilder()
+				.description("API <style>.models {display: none !important}</style>") //Para que os models nao aparecam no rodape
+				.title("Corre application API")
+				.version("v1")
+				.build();
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(info);
+	}
+
+}
